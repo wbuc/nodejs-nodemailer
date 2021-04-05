65ac67aef125feea28b1c568a028431feccd2825
wbuc <wbuchling@live.com>
2021-04-05 19:32:35 +0100

    Incldued now the helper approach as well.

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const nodemailer = require("nodemailer");
 const MailService = require("./MailService");
 const mailService = new MailService();
+const mailHelper = require("./mailHelper");
 const app = express();
 const port = 4000;
 
-
+/# Class approach #/;
 app.get("/", async (req, res) => {
     try {
         const order = {
@@ -29,6 +30,36 @@ app.get("/", async (req, res) => {
 
         await mailService.sendMail(mailInfo);
         res.send("email sent");
+
+    } catch (e) {
+        console.log(e);
+        res.status(500).send("Something broke!");
+    }
+});
+/# Helper approach #/;
+app.get("/v1", async (req, res) => {
+    try {
+        const order = {
+            orderId: 948584,
+            name: "Patrik",
+            price: 100
+        };
+
+        const mailInfo = {
+            to: "test943933@test.com",
+            subject: "Order Confirmation v1",
+            template: "orderConfirmation",
+            context: order,
+            attachments: [
+                {
+                    filename: "order-detail.jpg",
+                    path: "./order-detail.jpg"
+                }
+            ]
+        };
+
+        await mailHelper.sendMail(mailInfo);
+        res.send("email sent from helper!");
 
     } catch (e) {
         console.log(e);
---
+++ mailHelper.js
@@ -0,0 +1,57 @@
+const nodemailer = require("nodemailer");
+const hbs = require("nodemailer-express-handlebars");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const {
+    SMTP_HOST,
+    SMTP_PORT,
+    SMTP_USER,
+    SMTP_PASSWORD,
+    FROM_EMAIL
+} = process.env;
+
+/# Nodemailer config #/;
+const transporter = nodemailer.createTransport({
+    host: SMTP_HOST,
+    port: SMTP_PORT,
+    auth: {
+        user: SMTP_USER,
+        pass: SMTP_PASSWORD
+    }
+});
+
+/# Handlebar config #/;
+const options = {
+    viewEngine: {
+        partialsDir: __dirname + "/views/partials",
+        layoutsDir: __dirname + "/views/layouts",
+        extname: ".hbs"
+    },
+    extName: ".hbs",
+    viewPath: "views"
+};
+
+transporter.use("compile", hbs(options));
+
+module.exports = {
+    sendMail(
+        {
+            to,
+            subject,
+            template,
+            context,
+            attachments
+        }
+    ) {
+        return transporter.sendMail({
+            to,
+            from: FROM_EMAIL,
+            subject,
+            template,
+            context,
+            attachments
+        });
+    }
+}
